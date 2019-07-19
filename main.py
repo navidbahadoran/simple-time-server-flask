92a81f2fae38206e782c71091d0b9708cda036da
Navid Bahadoran <navid.bahadoran@gmail.com>
2019-07-19 13:07:16 -0700

    initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+from flask import Flask
+import os
+import time
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def show_epoch():
+    return str(time.time())
+
+
+if __name__ == '__main__':
+    port = os.environ.get('PORT', 6732)
+    app.run(host='0.0.0.0', port=port, debug=True)
