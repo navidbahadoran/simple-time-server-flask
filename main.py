7f2323c669d336700740564d2f195a3867f72c4f
Navid Bahadoran <navid.bahadoran@gmail.com>
2019-07-20 13:32:41 -0700

    final

--- main.py
+++ main.py
@@ -12,5 +12,5 @@ def show_epoch():
 
 if __name__ == '__main__':
     port = os.environ.get('PORT', 6732)
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host='0.0.0.0', port=port)
 
